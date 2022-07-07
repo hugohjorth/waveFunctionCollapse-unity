87d9af1efab4ba6a183b1cd318f4ef1ee45f5ef8
abbhughjo <43879922+abbhughjo@users.noreply.github.com>
2022-07-07 23:57:01 +0200

    created base game

---
+++ pokemaine/Assets/Scrips/PlayerMovment.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        //Inputs
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
